cf19b8e664c77abd8399eb6a94107c39e1a2a328
Dishal Dasanayake <dasanayakedishal@gmail.com>
2025-05-08 05:57:23 +0530

    refactor Post entity to include Lombok annotations and table mapping

--- backend/post-service/src/main/java/com/postservice/postservice/Domain/entity/Post.java
+++ backend/post-service/src/main/java/com/postservice/postservice/Domain/entity/Post.java
@@ -11,8 +11,20 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Table(name = "posts")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Post {
     @Id
     private Long id;
