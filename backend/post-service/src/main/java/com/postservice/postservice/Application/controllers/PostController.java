7a6541a05a89b635be76177dc2234d0942dcd5ec
Dishal Dasanayake <dasanayakedishal@gmail.com>
2025-05-08 06:13:40 +0530

    refactor PostController to implement post retrieval logic and enhance structure

--- backend/post-service/src/main/java/com/postservice/postservice/Application/controllers/PostController.java
+++ backend/post-service/src/main/java/com/postservice/postservice/Application/controllers/PostController.java
@@ -2,24 +2,30 @@ package com.postservice.postservice.Application.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import com.postservice.postservice.External.repository.PostRepository;
+import lombok.AllArgsConstructor;
+import java.util.Optional;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-
-
 @RestController
 @RequestMapping("/api/posts")
+@AllArgsConstructor
 public class PostController {
+
+    private PostRepository postRepository;
 
     @GetMapping("/")
     public String getPosts() {
-        return "Posts";
+        Optional<String> posts = postRepository.findAll().stream()
+                .map(post -> post.getDescription())
+                .reduce((first, second) -> first + ", " + second);
+        return posts.orElse("No posts found");
     }
 
     @PostMapping("/add")
     public String postMethodName(@RequestBody String entity) {
-        //TODO: process POST request
         
         return entity;
     }
