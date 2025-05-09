77f0baa3cdd456ec491ef8a5c32759ec1f2dd115
Dishal Dasanayake <dasanayakedishal@gmail.com>
2025-05-09 17:53:36 +0530

    feat: add job management routes to App component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import PostDetail from './pages/PostDetail';
-import './styles/global.css';
-import './styles/components.css';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import PostDetail from "./pages/PostDetail";
+import JobsPage from "./pages/JobHomePage";
+import JobDetailsPage from "./pages/JobDetailsPage";
+import "./styles/global.css";
+import "./styles/components.css";
 
 function App() {
   return (
@@ -11,6 +13,8 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/post/:id" element={<PostDetail />} />
+        <Route path="/jobs" element={<JobsPage />} />
+        <Route path="/jobs/:id" element={<JobDetailsPage />} />
       </Routes>
     </Router>
   );
