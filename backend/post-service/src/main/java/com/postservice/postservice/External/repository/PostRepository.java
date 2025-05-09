4acf0bd962652054bc25b07950900b96b0cbe51d
Dishal Dasanayake <dasanayakedishal@gmail.com>
2025-05-10 00:15:10 +0530

    add method to retrieve posts by user ID in PostRepository

--- backend/post-service/src/main/java/com/postservice/postservice/External/repository/PostRepository.java
+++ backend/post-service/src/main/java/com/postservice/postservice/External/repository/PostRepository.java
@@ -1,10 +1,14 @@
 package com.postservice.postservice.External.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.postservice.postservice.Domain.entity.Post;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
+
+    List<Post> findAllByUserId(String userId);
 
 }
