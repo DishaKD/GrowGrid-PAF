23ef52322976f8bae1af13cb1a15a19c5d5db96e
Dishal Dasanayake <dasanayakedishal@gmail.com>
2025-05-10 01:52:52 +0530

    rename getJobById to getPostById for consistency in postService.js

--- frontend/src/services/postService.js
+++ frontend/src/services/postService.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const API_URL = "http://localhost:8090/api/posts";
 
 const getAllPosts = () => axios.get(`${API_URL}/`);
-const getJobById = (id) =>
+const getPostById = (id) =>
   axios.get(`${API_URL}/${id}`).then((res) => res.data);
 const createJob = (jobData) => axios.post(API_URL, jobData);
 const updateJob = (id, jobData) => axios.put(`${API_URL}/${id}`, jobData);
@@ -11,7 +11,7 @@ const deleteJob = (id) => axios.delete(`${API_URL}/${id}`);
 
 export default {
   getAllPosts,
-  getJobById,
+  getPostById,
   createJob,
   updateJob,
   deleteJob,
