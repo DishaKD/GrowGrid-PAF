871b9c288eab189c8726d58be1c71f730d2da2e8
Dishal Dasanayake <dasanayakedishal@gmail.com>
2025-05-09 19:17:54 +0530

    feat: implement post service with CRUD operations

---
+++ frontend/src/services/postService.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+
+const API_URL = "http://localhost:8090/api/posts";
+
+const getAllPosts = () => axios.get(`${API_URL}/`);
+const getJobById = (id) =>
+  axios.get(`${API_URL}/${id}`).then((res) => res.data);
+const createJob = (jobData) => axios.post(API_URL, jobData);
+const updateJob = (id, jobData) => axios.put(`${API_URL}/${id}`, jobData);
+const deleteJob = (id) => axios.delete(`${API_URL}/${id}`);
+
+export default {
+  getAllPosts,
+  getJobById,
+  createJob,
+  updateJob,
+  deleteJob,
+};
