401e40b7e6f5fe034df09ee6e54c20c54dff968f
Prabodha <dinushipathiraja1@gmail.com>
2025-05-09 21:53:31 +0530

    add create-job and edit-job routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,8 @@ import HomePage from "./pages/HomePage";
 import PostDetail from "./pages/PostDetail";
 import JobsPage from "./pages/JobHomePage";
 import JobDetailsPage from "./pages/JobDetailsPage";
+import JobCreatePage from "./pages/CreateJobPage";
+import JobEditPage from "./pages/EditJobPage";
 import "./styles/global.css";
 import "./styles/components.css";
 
@@ -15,6 +17,8 @@ function App() {
         <Route path="/post/:id" element={<PostDetail />} />
         <Route path="/jobs" element={<JobsPage />} />
         <Route path="/jobs/:id" element={<JobDetailsPage />} />
+        <Route path="/create-job" element={<JobCreatePage />} />
+        <Route path="/jobs/:id/edit" element={<JobEditPage />} />
       </Routes>
     </Router>
   );
