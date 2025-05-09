9d3d7b1ec7b1d6749baba6d5172a85a4310b78bc
Dishal Dasanayake <dasanayakedishal@gmail.com>
2025-05-10 00:14:32 +0530

    add resource handler configuration for file uploads

--- backend/post-service/src/main/java/com/postservice/postservice/PostserviceApplication.java
+++ backend/post-service/src/main/java/com/postservice/postservice/PostserviceApplication.java
@@ -2,12 +2,24 @@ package com.postservice.postservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class PostserviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PostserviceApplication.class, args);
+	}
+
+	@Configuration
+	public class WebConfig implements WebMvcConfigurer {
+		@Override
+		public void addResourceHandlers(ResourceHandlerRegistry registry) {
+			registry.addResourceHandler("/uploads/**")
+					.addResourceLocations("file:uploads/");
+		}
 	}
 
 }
