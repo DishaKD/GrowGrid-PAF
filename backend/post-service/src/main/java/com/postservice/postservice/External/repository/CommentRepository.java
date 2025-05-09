675802a1358c9bd81aed16d763f340419ca97141
DishaKD <dasanayakedishal@gmail.com>
2025-05-10 01:15:59 +0530

    add CommentRepository for managing comments in the database

---
+++ backend/post-service/src/main/java/com/postservice/postservice/External/repository/CommentRepository.java
@@ -0,0 +1,14 @@
+package com.postservice.postservice.External.repository;
+
+import com.postservice.postservice.Domain.entity.Comment;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findByPostId(Long postId);
+
+    void deleteByPostId(Long postId);
+
+    void deleteById(Long commentId);
+}
