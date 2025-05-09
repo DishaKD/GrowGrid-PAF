d0920ee7a83503c276b2d923f9c0d827658f5255
Dishal Dasanayake <dasanayakedishal@gmail.com>
2025-05-10 03:06:42 +0530

    refactor postService to consolidate job-related functions and enhance media handling

--- frontend/src/services/postService.js
+++ frontend/src/services/postService.js
@@ -3,16 +3,45 @@ import axios from "axios";
 const API_URL = "http://localhost:8090/api/posts";
 
 const getAllPosts = () => axios.get(`${API_URL}/`);
+
 const getPostById = (id) =>
   axios.get(`${API_URL}/${id}`).then((res) => res.data);
-const createJob = (jobData) => axios.post(API_URL, jobData);
-const updateJob = (id, jobData) => axios.put(`${API_URL}/${id}`, jobData);
-const deleteJob = (id) => axios.delete(`${API_URL}/${id}`);
+
+const getPostsByUser = (userId) =>
+  axios.get(`${API_URL}/user/${userId}`).then((res) => res.data);
+
+const createPost = (formData) =>
+  axios.post(`${API_URL}/`, formData, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
+
+const updatePost = (id, formData) =>
+  axios.put(`${API_URL}/${id}`, formData, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
+
+const deletePost = (id) => axios.delete(`${API_URL}/${id}`);
+
+const uploadMediaFile = (file) => {
+  const formData = new FormData();
+  formData.append("file", file);
+  return axios.post(`${API_URL}/upload`, formData, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+    },
+  });
+};
 
 export default {
   getAllPosts,
   getPostById,
-  createJob,
-  updateJob,
-  deleteJob,
+  getPostsByUser,
+  createPost,
+  updatePost,
+  deletePost,
+  uploadMediaFile,
 };
